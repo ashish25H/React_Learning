93ebe4805fb02306177ce30b2948ffdfd501e648
ashish25H <me.ashishkp@gmail.com>
2026-02-15 00:01:49 +0530

    add new project

---
+++ Router-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ Router-project/src/components/Github/Github.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+export default function GitHub() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://api.github.com/users/hiteshchoudhary')
+    .then(res => res.json())
+    .then(data => {
+      setData(data);
+    });
+  }, [])
+  return(
+    <div>Github: {data.followers}</div>
+  )
+}
