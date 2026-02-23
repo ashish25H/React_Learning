75302932d075d02c1d35a48e44777456065dd528
ashish25H <me.ashishkp@gmail.com>
2026-02-23 22:33:59 +0530

    start new project

---
+++ minicontextProject/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+     
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ minicontextProject/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import UserContext from "../context/UserContext";
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login;
